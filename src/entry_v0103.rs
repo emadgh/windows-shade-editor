9604a67ac277ddde9be24da33e4a92845226e331
Emad Ghasemi <emadghasemi_2006@yahoo.com>
2026-08-13 17:55:53 +0330

    Fix generated source include

--- src/entry_v0103.rs
+++ src/entry_v0103.rs
@@ -1,3 +1,3 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-include!(concat!(env("OUT_DIR"), "/app_main_v0103.rs"));
+include!(concat!(env!("OUT_DIR"), "/app_main_v0103.rs"));
