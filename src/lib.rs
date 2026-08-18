15f7109bca8aa56ee8dfc6598f3db29c897bbed7
Emad Ghasemi <emadghasemi_2006@yahoo.com>
2026-08-18 15:10:35 +0330

    Expose inverse LUT path validation

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,7 @@ pub mod inverse_lut_continuity_builder;
 pub mod inverse_lut_continuity_field;
 pub mod inverse_lut_holdout;
 pub mod inverse_lut_identity;
+pub mod inverse_lut_path_validation;
 pub mod inverse_lut_runtime;
 pub mod inverse_lut_validation;
 pub mod inverse_lut_validation_eval;
